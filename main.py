d047dcd0ca569cd4f6e8103636166959e4237dac
Gordon Burns <gordonburns@live.co.uk>
2023-07-21 13:59:09 +0100

    added send email functionality

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import selectorlib
+import smtplib
+import ssl
+import os
+import creds
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 
@@ -17,8 +21,19 @@ def extract(source):
     return value
 
 
-def send_email():
-    print("Email was sent!")
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = creds.getusername()
+    password = creds.get_password()
+    receiver = creds.get_receiver()
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 
 def store(extracted):
@@ -40,4 +55,4 @@ if __name__ == "__main__":
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(message =f"Hey, New event was found \n {extracted}")
